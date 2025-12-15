ad1107cd4511bfb00bbf78d1167f422d7fbe98fd
Bariwu Alhassan Nyankpani <bariwunyankpani@gmail.com>
2025-12-15 17:37:01 +0000

    User Registration serializer, views and url pattern implementation

--- users/serializers.py
+++ users/serializers.py
@@ -6,7 +6,7 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
 
   class Meta:
     model = User
-    fields = ('id', 'username', 'email', 'password')
+    fields = ['id', 'username', 'email', 'password']
 
   
   def create(self, validated_data):
--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,6 @@ from rest_framework.authtoken.views import obtain_auth_token
 from .views import RegisterView
 
 urlpatterns = [
-    path("register/", RegisterView.as_view(), name='register')
+    path("register/", RegisterView.as_view(), name='register'),
+    path('login/', obtain_auth_token, name='login'),
 ]
