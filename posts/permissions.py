5164ba5c6c86417ddcd387ac223161423e964dbf
Bariwu Alhassan Nyankpani <bariwunyankpani@gmail.com>
2025-12-16 11:48:09 +0000

    Blug Post CRUD. 1. Implementation of blog post creation(CREATE) 2. Implementation of permissions for (GET,PUT,UPDATE,DELETE) 3. Automatic assignment of the post author from the authentication token. 4. Only authenticated users can Create, Update or Delete Posts. 5. If the request from the client is not POST then any user is allowed.

---
+++ posts/permissions.py
@@ -0,0 +1,15 @@
+from rest_framework import permissions
+
+class IsAuthorOrReadOnly(permissions.BasePermission):
+  """
+  Custom permission to only allow authors of an object to edit or delete it.
+  Read permissions are allowed to any request.
+  """
+  def has_object_permission(self, request, view, object):
+    # Allow GET, HEAD, or OPTIONS requests (Read-only access) for anyone
+    if request.method in permissions.SAFE_METHODS:
+      return True
+    
+    # Write permissions are only allowed to the author of the post.
+    # obj is the Post instance retrieved by the view.
+    return object.author == request.user
--- posts/views.py
+++ posts/views.py
@@ -2,18 +2,32 @@ from django.shortcuts import render
 from rest_framework import generics, permissions
 from .models import Post
 from .serializers import PostSerializer
+from rest_framework.permissions import IsAuthenticated
+from .permissions import IsAuthorOrReadOnly
 
 # Create your views here.
 #View for listing all posts and creating new posts
 class PostListCreateView(generics.ListCreateAPIView):
   queryset = Post.objects.all().order_by('-created_at') #Oder by newest first
   serializer_class = PostSerializer
-  #Allow any user to view the list of posts
-  permission_classes = [permissions.AllowAny]
+  
+  #Only Authenticated users can CREATE posts
+  def get_permissions(self):
+    if self.request.method == 'POST':
+      #Require authentication for the POST (Creation)
+      return [IsAuthenticated]
+    #Allow anyone to view the list (GET)
+    return [permissions.AllowAny()]
+  
+  def perform_create(self, serializer):
+    #Called right before post object is saved, sets the author to the logged-in User
+    serializer.save(author=self.request.user)
 
 #View for retrieving a single post (Read) and updating/deleting 
 class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
   queryset = Post.objects.all()
   serializer_class = PostSerializer
-  #Allow any user to view the details of a post
-  permission_classes = [permissions.AllowAny]
+  
+  # 1. User must be logged in (IsAuthenticated) to attempt modification.
+  # 2. They must pass the custom check (IsAuthorOrReadOnly).
+  permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
