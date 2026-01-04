72df396135d92e893cf9aa777afe08516e64299a
Bariwu Alhassan Nyankpani <bariwunyankpani@gmail.com>
2026-01-04 16:24:34 +0000

    Vercel deployment configurations

--- blogging_platform_api/urls.py
+++ blogging_platform_api/urls.py
@@ -19,10 +19,15 @@ from django.urls import path, include
 #IMAGEFIELD IMPORTS FOR DISPLAYING PROFILE PICTURE
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import JsonResponse
 
-
+def home(request):
+    return JsonResponse({
+        "message": "Blogging Platform API is running!"
+    })
 
 urlpatterns = [
+    path("", home),
     path('admin/', admin.site.urls),
     path('api/', include('users.urls')),
     path('api/', include('posts.urls')),
