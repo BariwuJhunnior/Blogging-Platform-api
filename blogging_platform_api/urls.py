91ae25df6b7fc52c40b414b7ec0b09f453095231
Bariwu Alhassan Nyankpani <bariwunyankpani@gmail.com>
2026-01-27 13:37:52 +0000

    Improved routes for the API endpoints

--- blogging_platform_api/urls.py
+++ blogging_platform_api/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for blogging_platform_api project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 #IMAGEFIELD IMPORTS FOR DISPLAYING PROFILE PICTURE
@@ -26,8 +10,23 @@ def home(request):
         "message": "Blogging Platform API is running!"
     })
 
+def api_root(request):
+    return JsonResponse({
+        "message": "Welcome to the Blogging Platform API",
+        "endpoints": {
+            "users": "/api/users/",
+            "register": "/api/register/",
+            "login": "/api/login/",
+            "posts": "/api/posts/",
+            "feed": "/api/feed/",
+            "docs": "/api/docs/swagger"
+        }
+    })
+
+
 urlpatterns = [
     path("", home),
+    path("api/", api_root),
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')),
     path('api/posts/', include('posts.urls')),
